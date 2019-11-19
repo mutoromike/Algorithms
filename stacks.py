6de5c2345e4fc8586f2a4534093a40da7b5b97b0
strongestavenger <mikemutoro@gmail.com>
2019-11-19 10:41:59 +0300

    add stacks functionality file

---
+++ stacks.py
@@ -0,0 +1,38 @@
+"""
+Implementing a STACK Data Structure
+"""
+
+class Stack():
+    """
+    a destructure that creates the initial Stack
+    """
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        """
+        Add item to stack
+        """
+        return self.items.append(item)
+
+    def pop(self):
+        """
+        Remove last item on stack
+        """
+        return self.items.pop()
+
+    def is_empty(self):
+        """
+        Check if stack is empty
+        """
+        return self.items == []
+
+    def peek(self):
+        """
+        Return the last item in the stack
+        """
+        if not self.is_empty():
+            return self.items[-1]
+    def get_stack(self):
+        return self.items
